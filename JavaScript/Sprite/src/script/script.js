8dfae730dd09584e5b12871438729de186206fa7
takedakenoutan <takedajojo@yahoo.co.jp>
2017-04-04 09:27:59 +0900

    JsDoc

--- JavaScript/Sprite/src/script/script.js
+++ JavaScript/Sprite/src/script/script.js
@@ -1,47 +1,70 @@
-/*
-Spriteコンストラクタ
-canvasとそのコンテキストを取得
-カウンタを初期化
-*/
-var Sprite = function(){
-	this.canvas = document.getElementById("Sprite");
+/**
+ * @fileoverview スプライトオブジェクトをつくるためのJavaScript
+ */
+
+/**
+ * スプライトオブジェクトを作るクラス
+ * メンバ変数の初期化を行う
+ * @constructor
+ * @param {string} id htmlのcanvasタグのID
+ * @type {EventTarget} canvas canvasタグ
+ * @type {Object} ctx canvasのコンテキスト
+ * @type {number} count フレームのカウンタ
+ * @type {isLoading} boolean ロード中か否か
+ * @type {EventTarget} img imageタグ
+ */
+
+var Sprite = function(id){
+	this.canvas = document.getElementById(id);
+	if(this.canvas === null){
+		throw new Error(id + "is not found.");
+	}
 	this.ctx = this.canvas.getContext("2d");
-	this.count = 0;
+	this.count = null;
+	this.isLoading = null;
+	this.img = null;
 };
 
-/*
-createメソッド
-isLoading : ロードのステータス
-img : 画像の情報
-*/
+/**
+ * スプライト画像から画像オブジェクトを作るメソッド
+ * @param {string} img スプライト画像のパス
+ */
+
 Sprite.prototype.create = function(img){
 	this.isLoading = true;
 	this.img = new Image();
-	this.img.src = img;
-	this.img.onload = (function(){
+	this.img.addEventListener("load", (function(){
 		this.isLoading = false;
-	}).bind(this);
+	}).bind(this), false);
+	this.img.addEventListener("error", (function(){
+		this.isLoading = null;
+		throw new Error(img + " is not Image.");
+	}).bind(this), false);
+	this.img.src = img;
 };
 
-/*
-animationメソッド
-引数たち
-loopFrame : コマの数
-imgX, imgY : 切り取る画像の座標
-width, height 画像サイズ
-canvasX, canvasY : 描画する座標
-type :{
-	loop : ループ
-	click : クリックで再描画
-}
-infinity : {
-	true : 無限ループ
-	false : コマ数描画して終了
-}
+/**
+ * アニメーションを動かすメソッド
+ * @param {number} loopFrame ループフレームの数
+ * @param {number} imgX 切り取る画像のX座標
+ * @param {number} imgY 切り取る画像のY座標
+ * @param {number} width 表示する画像の横幅
+ * @param {number} height 表示する画像の縦幅
+ * @param {number} canvasX 画像を表示するX座標
+ * @param {number} canvasY 画像を表示するY座標
+ * @param {string} アニメーションのタイプ
+ * @param {boolean} 無限か否か
+ */
 
-callback :実際に描画する
-*/
 Sprite.prototype.animation = function(loopFrame, imgX, imgY, width, height, canvasX, canvasY, type, infinity){
+	this.count = 0;
+	if(loopFrame < 1){
+		throw new Error("loopFrame is more than 0.");
+	}else if(type !== "loop" && type !== "click"){
+		throw new Error("type is 'loop' or 'click'");
+	}else if((typeof infinity) !== "boolean"){
+		throw new Error("infinity is boolean");
+	}
 	let callback = function(imgX, imgY, width, height, canvasX, canvasY){
 		this.draw(imgX + (width * this.count), imgY, width, height, canvasX, canvasY);
 		this.count += 1;
@@ -74,12 +97,35 @@ Sprite.prototype.animation = function(loopFrame, imgX, imgY, width, height, canv
 	}
 };
 
+/**
+ * 実際に描画するメソッド
+ * @param {number} imgX 切り取る画像のX座標
+ * @param {number} imgY 切り取る画像のY座標
+ * @param {number} width 表示する画像の横幅
+ * @param {number} height 表示する画像の縦幅
+ * @param {number} canvasX 画像を表示するX座標
+ * @param {number} canvasY 画像を表示するY座標
+ */
+
 Sprite.prototype.draw = function(imgX, imgY, width, height, canvasX, canvasY){
-	if(this.isLoading === undefined){
-		return false;
+	if(imgX < 0){
+		throw new Error("imgX is more than 0.");
+	}else if(imgY < 0){
+		throw new Error("imgY is more than 0.");
+	}else if(width < 1){
+		throw new Error("width is more than 0.");
+	}else if(height < 1){
+		throw new Error("height is more than 0.");
+	}else if(canvasX < 0){
+		throw new Error("canvasX is more than 0.");
+	}else if(canvasY < 0){
+		throw new Error("canvasY is more than 0.");
+	}
+	if(this.isLoading === null){
+		throw new Error("this object must do create.");
 	}else if(this.isLoading === true){
 		setTimeout(this.draw.bind(this, imgX, imgY, width, height, canvasX, canvasY, width, height), 100);
-		return false;
+		return;
 	}else if(this.isLoading === false){
 		this.ctx.clearRect(canvasX, canvasY, width, height);
 		this.ctx.drawImage(this.img, imgX, imgY, width, height, canvasX, canvasY, width, height);
@@ -87,7 +133,7 @@ Sprite.prototype.draw = function(imgX, imgY, width, height, canvasX, canvasY){
 };
 
 window.addEventListener("load", function(){
-	var loadTakedake = new Sprite();
+	var loadTakedake = new Sprite("Sprite");
 	loadTakedake.create("src/img/128_20_new.png");
 	loadTakedake.animation(20, 0, 0, 128, 128, 176, 76, "loop", true);
 }, false);
