1704f645b8c9059c5fb456f20b80d94645a24d88
takedakenoutan <takedajojo@yahoo.co.jp>
2018-08-23 23:03:07 +0900

    とりあえずサーバー

---
+++ Ruby/Main/httpServer.rb
@@ -0,0 +1,16 @@
+require 'socket'
+server = TCPServer.new 8000
+loop do
+	client = server.accept
+	headers = []
+	while header = client.gets
+		break if header.chomp.empty?
+		headers << header.chomp
+	end
+p headers
+client.puts "HTTP/1.0 200 OK"
+client.puts "Content-Type: text/plain"
+client.puts
+client.puts "message body"
+client.close
+end
